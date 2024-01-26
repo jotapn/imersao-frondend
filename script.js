16a67f23267854a28170509ed6d068cb95dcd94b
Pádua Neto <paduacn@gmail.com>
2024-01-26 20:46:14 -0300

    Adicionando campo de artistas e integrando com API o campo de busca

---
+++ script.js
@@ -0,0 +1,33 @@
+const searchInput = document.getElementById('search-input');
+const resultArtist = document.getElementById('result-artist');
+const resultPlaylist = document.getElementById('result-playlists');
+
+function requestApi(searchTerm) {
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    fetch(url)
+        .then((response) => response.json())
+        .then((results) => displayResults(results));
+}
+
+function displayResults(results) {
+    resultPlaylist.classList.add('hidden');
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
+
+    results.forEach((element) => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    });
+    resultArtist.classList.remove('hidden')
+}
+
+
+searchInput.addEventListener('input', function() {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === '') {
+        resultArtist.classList.add('hidden');
+        resultPlaylist.classList.remove('hidden');
+        return;
+    }
+    requestApi(searchTerm)
+});
